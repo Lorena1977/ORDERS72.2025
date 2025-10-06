0770fa5003e772db342560e79f4dbee15f018067
Lorena <lorelayda29hotmail.com>
2025-10-06 10:53:17 +0200

    Create CountriesIndex

---
+++ Orders72/Orders72.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using Orders72.Frontend.Repositories;
+using Orders72.Shared.Entities;
+
+namespace Orders72.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] 
+        private IRepository Repository { get; set; } = null!;
+
+        public List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var responseHppt = await Repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHppt.Response!;
+        }
+    }
+
+}
